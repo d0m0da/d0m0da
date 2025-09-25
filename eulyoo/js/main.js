2e58e679f459d83b5d0cad4af34ffeb6ab2ad977
d0m0da <ssssuunnn99@gmail.com>
2025-09-25 18:03:55 +0900

    250925_을유문화재단

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -48,8 +48,8 @@ $(document).ready(function(){
            처음에 로딩했을 때도 체크해야함
         --> 동일한 체크를 두번 실행 ==> 함수로 처리 */
 
-     let scrolling
-     function scroll_chk(){ // 함수를 정의한다
+    let scrolling
+    function scroll_chk(){ // 함수를 정의한다
         scrolling = $(window).scrollTop()
         // console.log(scrolling)
         if(scrolling > 0){
@@ -57,12 +57,34 @@ $(document).ready(function(){
         }else{
             $('header').removeClass('fixed')
         }
-     }
-     // 문서가 로딩되었을 때 단 1번 함수 실행
-     scroll_chk()
-     // 브라우저가 스크롤 할 때마다 1번씩 함수 실행
-     $(window).scroll(function(){
+    }
+    // 문서가 로딩되었을 때 단 1번 함수 실행
+    scroll_chk()
+    // 브라우저가 스크롤 할 때마다 1번씩 함수 실행
+    $(window).scroll(function(){
         scroll_chk()
-     })
+    })
+
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            451: {    /* 450px 이상일때 적용 */
+            slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            spaceBetween: 24,
+        },
+            769: {    /* 768px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 48,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        // loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+        navigation: {
+            nextEl: '.book .book_wrap .ctrl_btn button.btn_next',
+            prevEl: '.book .book_wrap .ctrl_btn button.btn_prev',
+        },
+    });
 
 })// 맨끝
