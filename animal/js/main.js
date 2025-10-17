1d553b997242636cea8e49731ba6cc1cbe512c11
d0m0da <ssssuunnn99@gmail.com>
2025-10-17 18:01:04 +0900

    251017_ 동물

--- animal/js/main.js
+++ animal/js/main.js
@@ -13,8 +13,9 @@ $(document).ready(function(){
         }else{
             device_status = 'mobile'
         }
-        console.log(device_status)
+        // console.log(device_status)
     }
+    
 
     device_chk() // html 로딩이 완료된 이후 단 1번 실행
     $(window).resize(function(){ // 브라우저가 리사이즈 될때마다 실행
@@ -84,11 +85,16 @@ $(document).ready(function(){
             // console.log('오버함')
             $('header').addClass('menu_pc')
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+            $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').hide()
             $(this).addClass('over')
+            $(this).find('.depth2').slideDown()
         }   
     })
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
-        $(this).removeClass('over')
+        if(device_status == 'pc'){ // pc일때만 동작
+            $(this).removeClass('over')
+            $(this).find('.depth2').hide()
+        }
     })
     $('header').on('mouseleave', function(){
         $(this).removeClass('menu_pc')
@@ -98,5 +104,116 @@ $(document).ready(function(){
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
     })
 
+
     /*************************** 시작: pc 버전 메뉴 오버 ******************************/
+    /*************************** 시작: 모바일 버전 1차메뉴 클릭 ******************************
+    /* 닫혀있는 메뉴를 클릭하면 기존에 열려있던 다른 메뉴를 닫고 나만 열기 (open 클래스 추가)
+    * 열려있는 메뉴를 클릭하면 나 자신을 닫고 끝남
+    * 열린 메뉴, 닫힌메뉴를 구분한는 방법 -- open 있으면 열린메뉴, 없으면 닫힌메뉴
+    * 1차 메뉴 a의 링크를 삭제 (링크 이동을 못하게 막음)*/
+
+
+    $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+        if(device_status == 'mobile'){
+            e.preventDefault()
+            
+            if($(this).parent().hasClass('open') == true){ // 열려있는 메뉴를 다시 클릭했을때
+                // console.log('열림')
+                $(this).parent().removeClass('open') // li에 open 클래스 삭제
+                $(this).next().slideUp() // 2차메뉴를 슬라이드로 닫기
+            }else{ // 열려있지 않은 다른 메뉴를 여는 것
+                // console.log('닫힘')
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open') // 모든 li의 open 클래스 삭제
+                $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp() // 모든 2차메뉴 닫기
+                $(this).parent().addClass('open')
+                $(this).next().slideDown() // 2차메뉴를 슬라이드로 열기
+            }
+        }
+    })
+
+
+    /*************************** 끝: 모바일 버전 1차메뉴 클릭 ******************************/
+    /*************************** 시작: 모바일 버전 메뉴 열기 ******************************/
+    /* 열기를 선택하면 header에 menu_mo 클래스 추가
+    * --> header .gnb .gnb_open
+    * 닫기를 클릭하면 header에 menu_mo 클래스 삭제
+    * --> header .gnb .gnb_wrap .gnb_close 
+    */
+
+    $('header .gnb .gnb_open').on('click', function(){
+        $('header').addClass('menu_mo')
+    })
+    $('header .gnb .gnb_wrap .gnb_close').on('click', function(){
+        $('header').removeClass('menu_mo')
+    })
+    /*************************** 끝: 모바일 버전 메뉴 열기 ******************************/
+
+    /*************************** 시작: 모바일 버전 메뉴 열기 ******************************/
+    /* pc / mobile 둘다
+    * 스크롤이 조금만 되어도 header에 fixed 클래스 추가
+    * 다시 맨위로 올라가면 header에 fixed 클래스 삭제
+    */
+
+    let scrolling // 스크롤된 값 저장
+
+    function scroll_chk(){
+        scrolling = $(window).scrollTop() // 현재 스크롤 값
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }
+
+    scroll_chk() // 문서가 로딩 되고 단 1번 실행
+    $(window).scroll(function(){
+        scroll_chk() // 스크롤 될때마다 1번씩 실행
+    })
+
+    /*************************** 끝: 모바일 버전 메뉴 열기 ******************************/
+
+    /*************************** 시작: 찾습니다 swiper ******************************/
+
+    const find1_swiper = new Swiper('.find .item1 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    
+        navigation: {
+            nextEl: '.find .item1 .next',
+            prevEl: '.find .item1 .prev',
+        },
+        
+    });
+
+    const find2_swiper = new Swiper('.find .item2 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    
+        navigation: {
+            nextEl: '.find .item2 .next',
+            prevEl: '.find .item2 .prev',
+        },
+        
+    });
+
+
+    /*************************** 끝: 찾습니다 swiper ******************************/
+
+
 });//맨끝
