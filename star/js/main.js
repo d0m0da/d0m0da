e8023e17cdd0cd014699cd9f7122565d954ef1ba
d0m0da <ssssuunnn99@gmail.com>
2025-11-14 11:29:03 +0900

    251128_으..

--- star/js/main.js
+++ star/js/main.js
@@ -47,16 +47,20 @@ $(document).ready(function () {
 
     // program swiper
     const program_swiper = new Swiper('.program .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 34,
+            },
+            1025: {    /* 640px 이상일때 적용 */
                 slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 53,
             },
         },
         centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         speed: 2000,
         autoplay: {  /* 팝업 자동 실행 */
             delay: 2000,
