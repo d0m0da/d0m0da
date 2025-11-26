7b3c06f9f9a7ad6536f43c15204bfee6318e5977
d0m0da <ssssuunnn99@gmail.com>
2025-11-26 18:03:33 +0900

    251126_ha

--- gunsan/js/main.js
+++ gunsan/js/main.js
@@ -20,44 +20,80 @@ $(document).ready(function(){
         },
     });
 
-    const concerts_swiper = new Swiper('.concerts .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 5, /* css에서 slide의 넓이ㅓ 지정 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+    // const concerts_swiper = new Swiper('.concerts .swiper', {
+    //     slidesPerView: 'auto',
+    //     spaceBetween: 16,
+    //     breakpoints: {
+    //         768: {    
+    //             spaceBetween: 40,
+    //         },
+    //     },
+    //     loop: true,
+    //     navigation: {
+    //         nextEl: '.control .btn_next',
+    //         prevEl: '.control .btn_prev',
+    //     },
+    //     on: {
+    //         init: function () {
+    //             updateProgress(this);
+    //         },
+    //         slideChange: function() {
+    //             const activeSlide = this.slides[this.activeIndex];
+    //             const activeSlideWidth = activeSlide.offsetWidth;
+    
+    //             const prevSlide = this.slides[this.previousIndex];
+    //             const prevSlideWidth = prevSlide.offsetWidth;
+    
+    //             const slideWidthDifference = activeSlideWidth - prevSlideWidth;
+    //             this.setTranslate(this.translate - slideWidthDifference);
+    
+    //             updateProgress(this);
+    //         },
+    //         slideChangeTransitionEnd: function() {
+    //             setTimeout(() => {
+    //                 this.update();
+    //             }, 10);
+    //         }
+    //     },
+    // });
+    
+    // function updateProgress(swiper) {
+    //     const total = swiper.slides.length - (swiper.loopedSlides * 2);
+    //     const current = swiper.realIndex + 1;
+    //     const percent = (current / total) * 100;
+    
+    //     document.querySelector('.progressbar span').style.width = percent + '%';
+    // }
+    const concerts_swiper = new Swiper('.concerts .swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 16,
         breakpoints: {
-            768: {    /* 768px 이상일때 적용 */
-                spaceBetween: 40,
-            },
+            768: { spaceBetween: 40 },
         },
-        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        // autoplay: {  /* 팝업 자동 실행 */
+        loop: true,
+        // autoplay: {
         //     delay: 2500,
         //     disableOnInteraction: true,
         // },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.concerts .swiper-pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            nextEl: '.concerts .btn_next',
+            prevEl: '.concerts .btn_prev',
         },
         on: {
-            slideChange: function() {
-                const activeSlide = this.slides[this.activeIndex]
-                const activeSlideWidth = activeSlide.offsetWidth
-                const otherSlides = this.slides[this.previousIndex]
-                const otherSlideWidth = otherSlides.offsetWidth			
-                const slideWidthDifference = activeSlideWidth - otherSlideWidth;
-                this.setTranslate(this.translate - slideWidthDifference);
+            init: function() {
+                updateProgress(this);
             },
-            slideChangeTransitionEnd: function() {
-                // 전환이 끝나면 Swiper를 다시 업데이트
-                setTimeout(() => {
-                    this.update();
-                }, 100);  // 잠시 딜레이를 주고 업데이트
-            }
+            slideChange: function() {
+                updateProgress(this);
+            },
         },
     });
+    
+    // progress bar 함수
+    function updateProgress(swiper) {
+        const totalSlides = swiper.slides.length - swiper.loopedSlides * 2; // 실제 슬라이드 개수
+        const currentSlide = swiper.realIndex + 1; // realIndex는 0부터 시작
+        const percent = (currentSlide / totalSlides) * 100;
+        document.querySelector('.progressbar span').style.width = percent + '%';
+    }
 })
