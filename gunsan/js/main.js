ecf7584ae6c7e174e1f0975302d4ecc2f61fc08b
d0m0da <ssssuunnn99@gmail.com>
2025-12-01 00:14:34 +0900

    251201_와12월

--- gunsan/js/main.js
+++ gunsan/js/main.js
@@ -332,25 +332,46 @@ $(document).ready(function(){
 
     //gallery
     const gallery_swiper = new Swiper('.gallery .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
+            769: {    /* 640px 이상일때 적용 */
                 slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 20,
+                spaceBetween: 24,
             },
         },
         //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 2500,
+        //     delay: 5000,
         //     disableOnInteraction: true,
         // },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.gallery .btn_next',
+            prevEl: '.gallery .btn_prev',
         },
     });
+
+    $('.gallery .tit .tap_list ul li').on('click', function(){
+        // 클릭한 li에만 active 클래스를 부여
+        $('.gallery .tit .tap_list ul li').removeClass('active')
+        $(this).addClass('active')
+
+        // 클릭한 li의 button에 선택됨이라고 글자쓰기
+        $('.gallery tit .tap_list ul li button span').text('')
+        $(this).find('button span').text('선택됨')
+
+        // 클릭한 li와 관련된 tab_contents tab_item 에 active 클래스 부여
+        tab_name = $(this).attr('data-tab')
+        // console.log(tab_name)
+        $('.gallery .tap_contents .tab_item').removeClass('active')
+        // find로 찾을때 클래스명이면 .이 추가되어있어야함, 내가 가져온 이름에 .이 없을때
+        $('.gallery .tap_contents').find('.' + tab_name).addClass('active')
+
+        // 선택된 .tab_item의 title에만 '선택됨'이라고 써주기
+        $('.gallery .tap_contents .tab_item').attr('title', '')
+        $('.gallery .tap_contents').find('.' + tab_name).attr('title', '선택됨')
+    })
 
     
 }) // 맨끝
